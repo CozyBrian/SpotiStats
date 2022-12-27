221fe64e78c9068b70edfead59410c3af628b925
CozyBrian <dragonbrian33@gmail.com>
2022-12-27 00:47:38 +0000

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <p className="text-green-600 text-7xl">THIS IS SPOTISTAT</p>
+    </div>
+  );
+}
+
+export default App;
